3b01259242531cad609842376de807700317c83b
anikmk <anikmollik122321@gmail.com>
2024-01-10 16:30:23 +0600

    make become a host route for guest

--- src/components/Dashboard/TableRows/GuestMenu.jsx
+++ src/components/Dashboard/TableRows/GuestMenu.jsx
@@ -1,19 +1,27 @@
-import { BsFillHouseAddFill } from "react-icons/bs";
-import MenuItem from "../Sidebar/MenuItem";
 
-
+import { BsFingerprint } from 'react-icons/bs'
+import { GrUserAdmin } from 'react-icons/gr'
+import useRole from '../../../hooks/useRole'
+import MenuItem from '../Sidebar/MenuItem'
 const GuestMenu = () => {
-    return (
-        <>
-        <MenuItem
-             icon={BsFillHouseAddFill}
-             label='My Booking'
-             address='myBooking'
+  const [role] = useRole()
+  return (
+    <>
+      <MenuItem
+        icon={BsFingerprint}
+        label='My Bookings'
+        address='my-bookings'
+      />
 
-           />
-           
-     </>
-    );
-};
+      {role === 'guest' && (
+        <div className='flex items-center px-4 py-2 mt-5  transition-colors duration-300 transform text-gray-600  hover:bg-gray-300   hover:text-gray-700 cursor-pointer'>
+          <GrUserAdmin className='w-5 h-5' />
 
-export default GuestMenu;
+          <span className='mx-4 font-medium'>Become A Host</span>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default GuestMenu
